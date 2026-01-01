1c26c1d0bbe99ed30b5c522de5fb7efecf9a0ddb
frankamente <fran29400@gmail.com>
2026-01-01 22:36:02 +0100

    chore: added mobile UI

--- pasoslargos-web/src/app/app.component.ts
+++ pasoslargos-web/src/app/app.component.ts
@@ -9,4 +9,23 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 })
 export class AppComponent {
   title = 'Pasos Largos';
+  isMenuOpen = false;
+  activeSubmenu: string | null = null;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    if (!this.isMenuOpen) {
+      this.activeSubmenu = null; // Reset submenus when closing main menu
+    }
+  }
+
+  toggleSubmenu(menu: string, event: Event) {
+    event.stopPropagation(); // Prevent bubbling if necessary
+    event.preventDefault(); // Prevent default anchor behavior if used on an anchor
+    if (this.activeSubmenu === menu) {
+      this.activeSubmenu = null;
+    } else {
+      this.activeSubmenu = menu;
+    }
+  }
 }
